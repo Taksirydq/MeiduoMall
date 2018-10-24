bd888375a8a0e6d6c3c1e3f1fc6b5c687e18f706
Taksirydq <1981438992@qq.com>
2018-10-24 08:51:54 +0800

    第三方工具包

---
+++ Meiduo/Meiduo/utils/ytx_sdk/sendSMS.py
@@ -0,0 +1,36 @@
+from .CCPRestSDK import REST
+
+
+class CCP:
+    @classmethod
+    def sendTemplateSMS(self, mobile, code,expires, template_id):
+        # 主帐号
+        accountSid = '8aaf070866196530016623366b8706d5'
+
+        # 主帐号Token
+        accountToken = 'fed35b19fa5748f682f03337799f8825'
+
+        # 应用Id
+        appId = '8aaf070866196530016623366bed06dc'
+
+        # 请求地址，格式如下，不需要写http://
+        serverIP = 'app.cloopen.com'
+
+        # 请求端口
+        serverPort = '8883'
+
+        # REST版本号
+        softVersion = '2013-12-26'
+
+        # 发送模板短信
+        # @param to 手机号码
+        # @param datas 内容数据 格式为列表 例如：[验证码，以分为单位的有效时间]
+        # @param $tempId 模板Id
+
+        # 初始化REST SDK
+        rest = REST(serverIP, serverPort, softVersion)
+        rest.setAccount(accountSid, accountToken)
+        rest.setAppId(appId)
+
+        result = rest.sendTemplateSMS(mobile, [str(code),str(expires)], template_id)
+        return result.get('statusCode')
