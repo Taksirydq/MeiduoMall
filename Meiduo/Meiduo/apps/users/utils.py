d45bfd85c9db89ee16630e64d891bd6d99908b70
Taksirydq <1981438992@qq.com>
2018-10-26 21:40:42 +0800

    自定义认证后端

--- Meiduo/Meiduo/apps/users/utils.py
+++ Meiduo/Meiduo/apps/users/utils.py
@@ -9,3 +9,25 @@ def jwt_response_payload_handler(token, user=None, request=None):
         'user_id': user.id,
         'username': user.username
     }
+
+
+class MyModelBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            # 判断username是用户名,还是手机号
+            if re.match(r'^1[3-9]\d{9}$', username):
+                # 手机号
+                user = User.objects.get(mobile=username)
+            else:
+                # 用户名
+                user = User.objects.get(username=username)
+        except:
+            # 没有查询到就返回空
+            return None
+
+        # 判断密码
+        if user.check_password(password):
+            # 返回user对象
+            return user
+        else:
+            return None
