8903a6b91a3d67171d57e37fe1c2f4630d5a07c4
Taksirydq <1981438992@qq.com>
2018-10-28 10:06:46 +0800

    使用celery发送短信

--- Meiduo/celery_tasks/sms/tasks.py
+++ Meiduo/celery_tasks/sms/tasks.py
@@ -4,5 +4,5 @@ from celery_tasks.main import app
 
 @app.task(name='send_sms_code')
 def send_sms_code(mobile, code, expires, template_id):
-    CCP.sendTemplateSMS(mobile, code, expires, template_id)
-
+    # CCP.sendTemplateSMS(mobile, code, expires, template_id)
+    pass
