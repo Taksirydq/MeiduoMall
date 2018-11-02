b55cf0f244e55dfcb2262282ed11ab2836786849
Taksirydq <1981438992@qq.com>
2018-11-02 14:20:30 +0800

    商品信息

--- Meiduo/Meiduo/apps/goods/urls.py
+++ Meiduo/Meiduo/apps/goods/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
+        url(r'^categories/(?P<category_id>\d+)/skus/$', views.SKUListView.as_view()),
 ]
--- Meiduo/Meiduo/apps/goods/views.py
+++ Meiduo/Meiduo/apps/goods/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import SKU
+from rest_framework.filters import OrderingFilter
+from utils.pagination import SKUListPagination
+from .serializers import SKUSerializer
 
-# Create your views here.
+
+class SKUListView(ListAPIView):
+    def get_queryset(self):
+        # 查询多个时, 获取路径中的参数: self.kwargs---->字典
+        return SKU.objects.filter(category_id=self.kwargs['category_id'])
+
+    serializer_class = SKUSerializer
+
+    # 分页
+    pagination_class = SKUListPagination
+
+    # 排序
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['create_time', 'price', 'sales']
