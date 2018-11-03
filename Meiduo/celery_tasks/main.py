f1a605a49ff569c0bedd01d432213d9b356a0a04
Taksirydq <1981438992@qq.com>
2018-11-03 11:51:15 +0800

    使用celery实现异步(页面静态化)

--- Meiduo/celery_tasks/main.py
+++ Meiduo/celery_tasks/main.py
@@ -14,7 +14,8 @@ app.config_from_object('celery_tasks.config')
 # 自动识别任务
 app.autodiscover_tasks([
     'celery_tasks.sms',
-    'celery_tasks.email'
+    'celery_tasks.email',
+    'celery_tasks.html'
 ])
 
 
